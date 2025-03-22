8465b8faef9a22fb1fc1d64b7f3c24d44f97d915
Abishek <np03cs4s220140@heraldcollege.edu.np>
2025-03-22 14:26:34 +0545

    store and cart function

--- projectDjango/settings.py
+++ projectDjango/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'templates',
     'accounts',
     'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'Ecommerce.context_preprocessor.category_context',
+                'cart.context_preprocessor.counter',
             ],
         },
     },
--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Variation
 
 # Register your models here.
 class productAdmin(admin.ModelAdmin):
@@ -8,6 +8,13 @@ class productAdmin(admin.ModelAdmin):
         'slug':('product_name',)
     }
     list_display_links=('modified_date','product_name')
+
+
+class varitationAdmin(admin.ModelAdmin):
+    list_display=('product','variation_category','varation_value','is_active','created_date')
+    list_editable=('is_active',)
+    list_filter=('product','variation_category','varation_value')
   
 
 admin.site.register(Product,productAdmin)
+admin.site.register(Variation,varitationAdmin)
